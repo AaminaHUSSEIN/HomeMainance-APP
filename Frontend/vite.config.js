b3b04da2bf4cc6cd0c607b6e2b281d9fa10bb1f3
Aamina Hussain <aaminahussein167@gmail.com>
2026-02-21 14:51:57 +0300

    Final fix for vite permissions

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5006',
-        changeOrigin: true,
-      },
-    },
-  },
+  build: {
+    outDir: 'dist', // Hubi in folder-kani uu yahay kan uu server.js raadinayo
+  }
 })
